0f826fa5fa9f6d930ee6c924bd1343170c09652d
shiwani08 <shiwanisoni29082002@gmail.com>
2025-10-27 10:03:06 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p = Person("Bing", 21)
+print(p.name, p.age)
+
+a = Person("Joey", 21)
+print(a.name, a.age)
