341e2ab0faab5e48817c01b66166fdd475231c8f
shiwani08 <shiwanisoni29082002@gmail.com>
2025-11-05 10:36:24 +0530

    Library

---
+++ practice/oops_solutions/08_library/books.py
@@ -0,0 +1,15 @@
+# from abc import ABC, abstractmethod
+from bookStatus import BookStatus
+class Books(BookStatus):
+
+    def __init__ (self, title, author, is_available):
+       pass
+
+    def status():
+        pass
+
+    def is_borrow():
+        pass
+
+    def show_details():
+        pass
